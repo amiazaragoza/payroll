cf6849614a8a45e2dc30cedf395d467988d32b38
Kcire325 <frederickjames27@gmail.com>
2025-02-19 14:06:29 +0800

    453345453345
    3453453453453

--- Source Code/PayrollSystem/src/MainPackage/LoginFrame.java
+++ Source Code/PayrollSystem/src/MainPackage/LoginFrame.java
@@ -959,9 +959,7 @@ public final class LoginFrame extends javax.swing.JFrame {
                 forgotpasswordBTNActionPerformed(evt);
             }
         });
-
-        pnlMAIN.add(forgotpasswordBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 430, 100, 20));
-
+        pnlMAIN.add(forgotpasswordBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 410, 110, 20));
 
         userlevelLABEL.setFont(new java.awt.Font("Microsoft Tai Le", 1, 10)); // NOI18N
         userlevelLABEL.setForeground(new java.awt.Color(102, 102, 102));
@@ -998,7 +996,7 @@ public final class LoginFrame extends javax.swing.JFrame {
                 clearBTNActionPerformed(evt);
             }
         });
-        pnlMAIN.add(clearBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 440, 40, 20));
+        pnlMAIN.add(clearBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 430, 40, 20));
 
         guiTitle.setFont(new java.awt.Font("Microsoft New Tai Lue", 1, 24)); // NOI18N
         guiTitle.setText("Log In to Your Account");
